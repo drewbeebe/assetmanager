2d86c824c408737007a16d371a32bf1fd1a81dd8
Drew Beebe <jdbeebe@gmail.com>
2022-07-21 10:14:30 -0400

    fixed

--- assetmanager/assetmanager/settings.py
+++ assetmanager/assetmanager/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t_8d&q_%8=495b=xfv_3x812&d8rf!xn00p&r$iyoxhsg@%%%#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
